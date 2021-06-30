eea3d1728d6f7ca2905b5ba889adce7cfdc3a37d
Daniel Norred <daniel@nor.red>
2021-06-29 20:33:05 -0500

    Manual File Importing

---
+++ src/Movie/Start-ManualMovieImport.ps1
@@ -0,0 +1,54 @@
+function Start-ManualMovieImport {
+    param (
+        [Alias("id")]
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName, ParameterSetName = 'id')]
+        [ValidateNotNullOrEmpty()] 
+        $path,
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName, ParameterSetName = "id")]
+        [ValidateNotNullOrEmpty()] 
+        $folderName,
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName, ParameterSetName = "id")]
+        [ValidateNotNullOrEmpty()] 
+        $movieId
+        
+    )
+    begin {
+        $connection = Get-RadarrConnection
+    }
+
+    process {
+        $Request = @{
+            Uri    = ($connection.URL + "/command")
+            Headers = @{
+                "x-api-key" = $connection.APIKey
+            }
+            ContentType = "application/json"
+            Body   =  @{
+                    name       = 'ManualImport'
+                    importMode = "auto"
+                    files      = @(
+                        @{
+                            path       = $path
+                            folderName = $folderName
+                            movieId    = $movieId
+                            quality    = @{
+                                "id"         = 0
+                                "name"       = "Unknown"
+                                "source"     = "unknown"
+                                "resolution" = 0
+                                "modifier"   = "none"
+                            }
+                            revision   = @{
+                                version = 0
+                                real    = 0
+                            }
+                        }
+                    )
+                    #Dear fucking god it doesn't actually convert to json properly in the request body unless you do this
+                } |ConvertTo-Json -Depth 5 -EnumsAsStrings | Out-String 
+            Method = "Post"
+        }
+
+        Invoke-RestMethod @Request
+    }
+}
